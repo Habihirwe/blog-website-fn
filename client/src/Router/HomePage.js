5acd0a2afe7d1c87c199234fe0e12cb0651848c4
unknown <jbniyikiza20@gmail.com>
2024-05-12 19:09:01 +0200

    initial Commit

---
+++ client/src/Router/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from '../components/Home.js';
+import Navbar from './Navbar.js';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* <h1>hello world</h1> */}
+       <Home/>
+    </div>
+  )
+} 
+
+export default  HomePage;
